cf851c18943f962c0b1d8e9ba18e47487cb3ea11
aestheticCheater <email>
2022-05-12 19:50:44 +0300

    Last commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+
+app.use(express.static("public"));
+
+
 let date_ob = new Date();
 // current date
 
@@ -31,7 +35,21 @@ app.post("/", function(req, res){
       const icon = weatherData.weather[0].icon;
       const description = weatherData.weather[0].description;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    res.write("<h1>The temperature in " + query + " is: " + temp  + " in <em>Celsius</em> at the moment.</h1>" + "<h1>"+ hours + ":" + minutes +"</h1>");
+         if(hours < 10 && minutes < 10){
+           res.write("<h1>The temperature in " + query + " is: " + temp  + " in <em>Celsius</em> at the moment.</h1>" + "<h1>"+ "0" + hours + ":0" + minutes +"</h1>");
+         }
+         else if(hours < 10 ){
+             res.write("<h1>The temperature in " + query + " is: " + temp  + " in <em>Celsius</em> at the moment.</h1>" + "<h1>"+ "0" + hours + ":" + minutes +"</h1>");
+         }
+      else if(minutes < 10){
+    res.write("<h1>The temperature in " + query + " is: " + temp  + " in <em>Celsius</em> at the moment.</h1>" + "<h1>"+ hours + ":0" + minutes +"</h1>");
+  }
+   else {
+     res.write("<h1>The temperature in " + query + " is: " + temp  + " in <em>Celsius</em> at the moment.</h1>" + "<h1>"+ hours + ":" + minutes +"</h1>");
+   }
+
+
+
     res.write("<img src =" + imageURL + ">");
      res.write("<center><h2>"+ description +"</h2></center>");
 
